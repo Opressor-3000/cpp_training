707ae5750cfacbc95fac8b475e238029e5612336
Opressor-3000 <9141727+Opressor-3000@users.noreply.github.com>
2025-11-21 19:57:19 +0300

    fix copy and move assignment

--- src/lessons_01_move/buffer/main.cpp
+++ src/lessons_01_move/buffer/main.cpp
@@ -13,6 +13,7 @@ TODO: реализуй:
     перемещающее присваивание (noexcept).
 */
 
+#include <concepts>
 #include <cstring>
 #include <utility>
 #include <cstddef>
@@ -20,35 +21,44 @@ TODO: реализуй:
 struct Buffer {
     size_t n{};
     char* data{};
-    
+public:
     explicit Buffer(size_t n): n(n), data(new char[n]{}) {}
-    Buffer (const Buffer& obj): n(obj.n), data(new char[*obj.data]) {
+
+    Buffer (const Buffer& obj): Buffer{obj.n} {
         std::memcpy(data, obj.data, n);
-     }
+    }
+
     Buffer (Buffer&& other) noexcept : n(other.n), data(other.data) {
         other.n = 0;
         other.data = nullptr;
     }
+
     Buffer& operator= (const Buffer& obj) {
-        if (this != &obj) 
-        {
-            delete[] data;
-            n = obj.n;
-            data = new char[n];
-            std::memcpy(data, obj.data, n);
-        }
+        
+        Buffer copy { obj };
+        swap(copy);
         return *this;
     }
+
     Buffer& operator= (Buffer&& other) noexcept {
         if(this != &other) 
         {
-            std::swap(n, other.n);
-            std::swap(data, other.data);
+            delete[] data;
+            n = other.n;
+            data = other.data;
+            other.data = nullptr;
+            other.n = 0;
         }
         return *this;
     }
-    ~Buffer() { delete[] data; }
 
+    void swap(Buffer& other) noexcept
+    {
+        std::swap(n, other.n);
+        std::swap(data, other.data);
+    }
+
+    ~Buffer() { delete[] data; }
 };
 
 int main() {
