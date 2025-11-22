81521b9c210bc72ede71b41a618621983ab92de5
Opressor-3000 <9141727+Opressor-3000@users.noreply.github.com>
2025-11-22 19:19:24 +0300

    noexcept influence on the std::vector task solution

--- src/lessons_01_move/noexcept/main.cpp
+++ src/lessons_01_move/noexcept/main.cpp
@@ -8,12 +8,13 @@
 
 #include <vector>
 #include <iostream>
+#include <utility>
 
 struct A {
     static inline int copies=0, moves=0;
     A() = default;
     A(const A&) { ++copies; }
-    A(A&&) /* попробуй без noexcept и с noexcept */ { ++moves; }
+    A(A&&) noexcept /* попробуй без noexcept и с noexcept */ { ++moves; }
 };
 
 int main() {
@@ -22,3 +23,17 @@ int main() {
     for (int i=0;i<100;i++) v.push_back(A{});
     std::cout << "copies=" << A::copies << " moves=" << A::moves << "\n";
 }
+
+/*
+Вывод:
+    если не использовать noexcept в КП то компилятор 
+    будет объект тк по умолчанию перемещение может выбросить исключение
+
+    если использовать noexcept то компилятор будет считать 
+    перемещение безопасным и использовать только его 
+
+    в сумме перемещений и копирований больше чем 100 тк 
+    при заполнении выделенной памяти под вектор происходила 
+    релокация на новую память 
+
+*/
