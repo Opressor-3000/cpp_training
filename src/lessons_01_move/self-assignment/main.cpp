d1f9cb3d182a0e6e1cce2d48264aca2db891c816
Opressor-3000 <9141727+Opressor-3000@users.noreply.github.com>
2025-11-23 19:39:02 +0300

    add self-assignment

---
+++ src/lessons_01_move/self-assignment/main.cpp
@@ -0,0 +1,28 @@
+/*
+Самоприсваивание перемещением
+Цель:
+    сделать перемещающее присваивание безопасным к obj = std::move(obj)
+Задание: 
+    Добавь проверки инварианта «пустого» состояния: 
+        если p==nullptr, объект валиден.
+*/
+
+#include <utility>
+
+struct Box {
+    int* p{};
+    Box(): p(new int(7)) {}
+    ~Box(){ delete p; }
+    Box(Box&& other) noexcept : p(other.p) { 
+        if (other.p != nullptr) other.p=nullptr;
+    }
+    Box& operator=(Box&& other) noexcept {
+        if (this != &other) { delete p; p = other.p; other.p=nullptr; }
+        return *this;
+    }
+};
+
+int main() {
+    Box b;
+    b = std::move(b);  // не должен падать/течь
+}
