ea0b5fb2617515b400b91ec65ffaa2f71b14c4fe
Opressor-3000 <9141727+Opressor-3000@users.noreply.github.com>
2025-12-04 13:12:33 +0300

    add increment template

--- src/lessons_04_templates/increment/main.cpp
+++ src/lessons_04_templates/increment/main.cpp
@@ -5,3 +5,40 @@
     3. Для остальных типов должна быть ошибка компиляции.
 */
 
+#include <utility>
+#include <type_traits>
+
+template<typename T>
+struct IsSame {static constexpr bool value = false;};
+
+template<typename T>
+typename std::enable_if<
+    std::is_integral<T>::value &&
+    !std::is_same<T, char>::value &&
+    !std::is_same<T, unsigned char>::value &&
+    !std::is_same<T, signed char>::value,
+    T
+>::type
+increment(T t) { return ++t; }
+
+int main(void)
+{
+    int i = 100000;
+    i = increment(i);
+    short s = 1000;
+    long l = 11111111111;
+#ifdef chars
+    char c = 100;
+    c = increment(c);
+    unsigned char uc = 11;
+    uc = increment(uc);
+#elif defined(bool)
+    bool t = true;
+    t = increment(t);
+#elif defined(enum)
+    enum A {
+        g = 1
+    };
+    A = increment(A);
+#endif
+}
