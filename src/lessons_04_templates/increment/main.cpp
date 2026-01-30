a561cac333379cbc19a8bc9b871d9fdc100b27f4
Opressor-3000 <9141727+Opressor-3000@users.noreply.github.com>
2026-01-30 12:45:44 +0300

    fix increment/main.cpp

--- src/lessons_04_templates/increment/main.cpp
+++ src/lessons_04_templates/increment/main.cpp
@@ -5,39 +5,31 @@
     3. Для остальных типов должна быть ошибка компиляции.
 */
 
-#include <utility>
 #include <type_traits>
 
 template<typename T>
-struct IsSame {static constexpr bool value = false;};
+requires (std::is_integral_v<T> && !std::is_same_v<T, bool>)
+T increment(T x)
+{
+    return x + 1;
+};
 
-template<typename T>
-typename std::enable_if_t<
-    std::is_integral<T>::value &&
-    !std::is_same<T, unsigned char>::value &&
-    !std::is_same<T, signed char>::value,
-    T
->
-increment(T t) { return ++t; }
+
+template <typename T>
+concept incrementTest = requires(T t) 
+{
+    increment(t);
+};
 
 int main(void)
 {
-    int i = 100000;
-    i = increment(i);
-    short s = 1000;
-    long l = 11111111111;
-#ifdef chars
-    char c = 100;
-    c = increment(c);
-    unsigned char uc = 11;
-    uc = increment(uc);
-#elif defined(bool)
-    bool t = true;
-    t = increment(t);
-#elif defined(enum)
-    enum A {
-        g = 1
-    };
-    A = increment(A);
-#endif
+    static_assert(incrementTest<int>);
+    static_assert(incrementTest<unsigned int>);
+    static_assert(incrementTest<char>);
+    static_assert(incrementTest<unsigned char>);
+    static_assert(!incrementTest<double>);
+    static_assert(!incrementTest<char[4]>);
+    static_assert(!incrementTest<int*>);
+    static_assert(incrementTest<unsigned long long>);
+
 }
