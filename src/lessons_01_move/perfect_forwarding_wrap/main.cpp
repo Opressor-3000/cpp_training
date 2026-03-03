31b94dcd36bdb652c94560d4d4dc93fcf037dbe4
Opressor-3000 <9141727+Opressor-3000@users.noreply.github.com>
2026-03-04 00:14:01 +0300

    add call func

--- src/lessons_01_move/perfect_forwarding_wrap/main.cpp
+++ src/lessons_01_move/perfect_forwarding_wrap/main.cpp
@@ -2,23 +2,29 @@
 Простая обёртка с perfect forwarding
 Цель: 
     сохранить категорию аргумента через std::forward.
+
 Задание: 
-    Убери `std::forward` и посмотри, что обе строки станут `lvalue`.
+    Убери std::forward и посмотри, что обе строки станут lvalue.
+
+decltype(auto)  -  позволяет сделать вывод типа возвращаемого занчения 
+                   для rvalue или lvalue выражений
 */
 
 #include <iostream>
+#include <utility>
+#include <type_traits>
+#include <functional>
 
 template <class F, class... Args>
 decltype(auto) call_forward(F&& f, Args&&... args) {
     return std::forward<F>(f)(std::forward<Args>(args)...);
 }
 
-#ifdef V1
 template <class F, class... Args>
-decltype(auto) call_forward(F&& f, Args&&... args) {
+std::invoke_result_t<F, Args...>
+call(F&& f, Args&&... args) {
     return f(args...);
 }
-#endif
 
 struct Probe {
     void operator()(int&)  const { std::cout << "lvalue\n"; }
@@ -27,11 +33,12 @@ struct Probe {
 
 int main() {
     Probe p;
+    Probe p1;
     int x = 0;
+    int y = 1;
     call_forward(p, x);        // lvalue
     call_forward(p, 0);        // rvalue
+    call(p1, y);
+    call(p1, 1);
+    return 0;
 }
-/*
-decltype(auto)  --  позволяет сделать вывод типа возвращаемого занчения 
-для rvalue или lvalue выражений
-*/
